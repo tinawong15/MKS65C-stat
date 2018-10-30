5bdb33851c7468aaa1814e223d44c8b5cbffd2a2
tina <tinaw425@gmail.com>
2018-10-30 19:32:37 -0400

    fixed ctime

--- main.c
+++ main.c
@@ -1,14 +1,17 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/stat.h>
+#include <time.h>
 
 int main() {
-  struct stat s;
-  stat("text.txt", &s);
-  printf("File Size: %lld\n", s.st_size);
-  printf("Permissions: %d\n", s.st_mode);
-  printf("Time of last access: %ld\n", s.st_atime);
+  struct stat *s = malloc(sizeof(struct stat));
+  stat("text.txt", s);
+  printf("File Size: %lld\n", s->st_size);
+  printf("Permissions: %o\n", s->st_mode);
+  printf("Time of last access: %s", ctime(&(s->st_atime)));
+  free(s);
   return 0;
 }
