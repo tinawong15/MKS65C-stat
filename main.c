0fe4837b76c81bc6ec7e1bf86b9a79e37d84f9b9
tina <tinaw425@gmail.com>
2018-10-30 22:32:14 -0400

    added byte conversion

--- main.c
+++ main.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
@@ -10,6 +9,13 @@ int main() {
   struct stat *s = malloc(sizeof(struct stat));
   stat("text.txt", s);
   printf("File Size: %lld\n", s->st_size);
+  char size[256];
+  int byte = s->st_size;
+  int kb = byte / 1000;
+  int mb = kb / 1000;
+  int gb = mb / 1000;
+  sprintf(size, "%d bytes\n%d KB\n%d MB\n%d GB", byte, kb, mb, gb);
+  printf("%s\n", size);
   printf("Permissions: %o\n", s->st_mode);
   printf("Time of last access: %s", ctime(&(s->st_atime)));
   free(s);
